62b21d3e7f8faacba40a63a157262e88576f8f67
unknown <nainapjj@617-0206.CEAS1.uc.edu>
2015-10-20 19:40:43 -0400

    Finished up find_s and fixed get_attribute_bins

---
+++ NameAss2.m
@@ -0,0 +1,40 @@
+bins_of_attributes = {};
+
+for i = 1:4
+    bins_of_attributes{1, length(bins_of_attributes) + 1} = get_attribute_bins(Data, i, 5);
+end
+
+%  Find_S for Setosa first
+h_s_setosa = find_s(Data, 1, 5, bins_of_attributes);
+
+%  Find_S for Versicolor first
+h_s_versicolor = find_s(Data, 2, 5, bins_of_attributes);
+
+%  Find_S for Virginica first
+h_s_virginica = find_s(Data, 3, 5, bins_of_attributes);
+
+% Classify an example in Setosa and not Setosa
+classify_example(h_s_setosa, [5.1, 3.5, 1.4, .2]) % setosa
+classify_example(h_s_setosa, [7, 3.2, 4.7, 1.4]) % versicolor
+
+% Classify an example for Versicolor
+classify_example(h_s_versicolor, [7, 3.2, 4.7, 1.4])
+
+% Classify an example for Virginica
+classify_example(h_s_virginica, [7.3, 2.9, 6.3, 1.8])
+
+% Check all of the values, with each of the hypotheses
+c_set = [];
+for i = 1:150
+    c_set(i) = classify_example(h_s_setosa, Data(i, 1:4));
+end
+
+c_ver = [];
+for i = 1:150
+    c_ver(i) = classify_example(h_s_versicolor, Data(i, 1:4));
+end
+
+c_vir = [];
+for i = 1:150
+    c_vir(i) = classify_example(h_s_virginica, Data(i, 1:4));
+end
---
+++ find_s.m
@@ -0,0 +1,39 @@
+function [ hypothesis ] = find_s( data_array, answer, answer_column, bins_of_attributes )
+%find_s The find_s function finds the most specific hypothesis
+%   That works with the concept space
+
+% Initialize the hypothesis to the nil h
+hypothesis = {};
+for i = 1:length(bins_of_attributes)
+    hypothesis{1, i} = {};
+end
+
+% Iterate through the data array
+for i = 1:length(data_array)
+    % Check to see if the example is a positive one
+    if data_array(i, answer_column) == answer
+        % Check to see if the example is in the hypothesis
+        for j = 1:length(bins_of_attributes)
+            is_in_hypothesis = false;
+            for k = 1:length(hypothesis{1, j})
+                if data_array(i, j) >= hypothesis{1,j}{1,k}(1) && data_array(i,j) <= hypothesis{1,j}{1,k}(2)
+                    is_in_hypothesis = true;
+                end
+            end
+            
+            % If not in hypothesis, then we have to make the hypothesis
+            % more general
+            if ~is_in_hypothesis
+                for k = 1:length(bins_of_attributes{1, j})
+                    if data_array(i, j) >= bins_of_attributes{1,j}{1,k}(1) && data_array(i,j) <= bins_of_attributes{1,j}{1,k}(2)
+                        hypothesis{1, j}{1, length(hypothesis{1, j}) + 1} = bins_of_attributes{1,j}{1,k};
+                    end
+                end
+            end 
+        end
+    end
+end
+
+
+end
+
