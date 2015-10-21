1ff8d080b8419231643c7318b0b0e3236f2c6d24
unknown <griffid6@548-0503.CEAS1.uc.edu>
2015-10-21 14:44:49 -0400

    Implemented candidate elimination algorithm for conjunctive hypothesis.

--- candidate_eliminiation.m
+++ candidate_eliminiation.m
@@ -1,15 +1,22 @@
 function [ s_hypothesis, g_hypothesis ] = candidate_eliminiation( data_array, answer, answer_column, bins_of_attributes )
 %candidate_eliminiation Implements the candidate elimination algorithm
-
-% Initialize the hypothesis to the nil h
+% Initialize the s_hypothesis to nil, and the g_hypothesis to the most
+% general with {-1} elements. 
 s_hypothesis = {};
+g_hypothesis = {};
+
+%Make initial hypotheses for s and g set.
+init_g = {};
+init_s = {};
 for i = 1:length(bins_of_attributes)
-    s_hypothesis{1, i} = {};
+    init_s{1, i} = {};
+    init_g{1, i} = {-1};
 end
 
-% Intialize the general hypothesis to be all bins
-g_hypothesis = {};
-g_hypothesis{1} = bins_of_attributes;
+%Since g is a set of hypotheses, set it to the hypotheses initially
+%created. 
+g_hypothesis{1, 1} = init_g;
+s_hypothesis = init_s;
 
 % Iterate through each data example
 for i = 1:length(data_array)
@@ -31,21 +38,48 @@ for i = 1:length(data_array)
         % Minimally generalize the s_hypothesis to be consistent with the
         % positive example
         % Check to see if the example is in the s_hypothesis
+        
+        
+        %Min generalize steps for conjunctive hypothesis:
+        %1) If attrib of hypothesis is {}, then add the bin that the
+        %example attrib value falls into.
+        %2) If attrib of hypothesis is a bin with two elements {#, #}, then
+        %set the attrib of the new hypothesis to {-1}
         for j = 1:length(bins_of_attributes)
             is_in_hypothesis = false;
-            for k = 1:length(s_hypothesis{1, j})
-                if data_array(i, j) >= s_hypothesis{1,j}{1,k}(1) && data_array(i,j) <= s_hypothesis{1,j}{1,k}(2)
-                    is_in_hypothesis = true;
-                end
+            
+            %Check if hypothesis attrib is {}, {-1}, or if the data
+            %value falls into the current hypothesis attrib bin. 
+            if isequal(s_hypothesis{1, j}, {})
+                is_in_hypothesis = false;
+            elseif isequal(s_hypothesis{1, j}, {-1})
+                is_in_hypothesis = true;
+            elseif data_array(i, j) >= s_hypothesis{1,j}{1,1}(1) && data_array(i,j) <= s_hypothesis{1,j}{1,1}(2)
+                is_in_hypothesis = true;
             end
             
             % If not in hypothesis, then minimally generalize the
             % s_hypothesis to include the positive example
             if ~is_in_hypothesis
-                for k = 1:length(bins_of_attributes{1, j})
-                    if data_array(i, j) >= bins_of_attributes{1,j}{1,k}(1) && data_array(i,j) <= bins_of_attributes{1,j}{1,k}(2)
-                        s_hypothesis{1, j}{1, length(s_hypothesis{1, j}) + 1} = bins_of_attributes{1,j}{1,k};
+                if isequal(s_hypothesis{1, j}, {-1})
+                    %If most general already, then continue on the process
+                    %the next attrib.
+                    continue
+                elseif isequal(s_hypothesis{1, j}, {})
+                    %If current attribute is empty, then find the bin
+                    %corresponding to the data element, and add it to
+                    %the hypothesis.
+                    for k = 1:length(bins_of_attributes{1, j})
+                        if data_array(i, j) >= bins_of_attributes{1,j}{1,k}(1) && data_array(i,j) <= bins_of_attributes{1,j}{1,k}(2)
+                            %s_hypothesis{1, j}{1, length(s_hypothesis{1, j}) + 1} = bins_of_attributes{1,j}{1,k};
+                            %Set the hypothesis attribute to the bin. 
+                            s_hypothesis{1, j} = {bins_of_attributes{1,j}{1,k}};
+                        end
                     end
+                else
+                    %If the hypothesis already has a bin, then
+                    %generalize that attribute to {-1}
+                     s_hypothesis{1, j} = {-1};
                 end
             end
         end
@@ -56,7 +90,7 @@ for i = 1:length(data_array)
             data_array(i)
         end
         
-        incosistent_hypotheses = [];
+        inconsistent_hypotheses = [];
         for hyp = 1:length(g_hypothesis)
             if classify_example(g_hypothesis{hyp}, data_array(i, 1:(answer_column-1)))
                 % Mark the incosistent hypothesis to be removed
@@ -74,41 +108,40 @@ for i = 1:length(data_array)
         % g_hypothesis
         for hyp = 1:length(incon_hyp)
             for attrb = 1:length(incon_hyp{1, hyp})
-                % Find the bin that matches with the negative example
-                % attribute fall into
-                for bin = 1:length(incon_hyp{1, hyp}{1, attrb})
-                    if data_array(i, attrb) >= incon_hyp{1, hyp}{1, attrb}{1, bin}(1) && data_array(i,attrb) <= incon_hyp{1, hyp}{1, attrb}{1, bin}(2)
-                        % Add to the g_hypothesis set the specialized hypothesis
-                        temp_hypothesis = incon_hyp{1, hyp};
-                        temp_hypothesis{1, attrb}(bin) = [];
-                        
-                        % Check to make sure the new general hypothesis is
-                        % more general than or equal to s_hypothesis
-                        if is_more_general_than_or_equal(temp_hypothesis, s_hypothesis)
-                            g_hypothesis{1, length(g_hypothesis) + 1} = temp_hypothesis;
+                
+                %If hypot attrib == {-1}, then specify.
+                %If hypot attrib == bin, and data value not in bin, then
+                %remove hypot. 
+                
+                if isequal(incon_hyp{1, hyp}{1, attrb}, {-1})
+                    %If hypot attrib == {-1}, then find corresponding bin and specify.
+                    for bin = 1:length(bins_of_attributes{1, attrb})
+                        if data_array(i, attrb) >= bins_of_attributes{1, attrb}{1,bin}(1) && data_array(i,attrb) <= bins_of_attributes{1, attrb}{1,bin}(2)
+                            continue
+                        else 
+                            temp_hypothesis = incon_hyp{1, hyp};
+                            temp_hypothesis{1, attrb} = {bins_of_attributes{1, attrb}{1,bin}};
+
+                            % Check to make sure the new general hypothesis is
+                            % more general than or equal to s_hypothesis
+                            if is_more_general_than_or_equal(temp_hypothesis, s_hypothesis)
+                                g_hypothesis{1, length(g_hypothesis) + 1} = temp_hypothesis;
+                            end
                         end
-                        break;
                     end
                 end
             end
         end
         
-        % Find all of the general hypotheses that specific forms of another
-        % general hypothesis.  Remove them!
-        redundant_hyp = [];
-        for hyp = 1:length(g_hypothesis)
-           for hyp_2 = (hyp+1):length(g_hypothesis)
-              if is_more_general_than_or_equal(g_hypothesis{1, hyp}, g_hypothesis{1, hyp_2})
-                 %disp('Found a more general hypothesis within the g_hypothesis space')
-                 redundant_hyp(length(redundant_hyp) + 1) = hyp_2;
-              end
-           end
-        end
-        g_hypothesis(redundant_hyp) = [];
+        % The algorithm specifies to find all of the general hypotheses that 
+        % specific forms of another general hypothesis.  
+        % We don't need to do this because of the way we form our 
+        % conjunctive hypothesis space.
     end
-    
-    i
 end
+
+s_hypothesis
+g_hypothesis
 
 
 end
--- is_more_general_than_or_equal.m
+++ is_more_general_than_or_equal.m
@@ -4,22 +4,15 @@ function [ is_general ] = is_more_general_than_or_equal( hypothesis_1, hypothesi
 
 % For each attribute i
 for i = 1:length(hypothesis_2)
-    % For each bin in hypothesis 2
-    for j = 1:length(hypothesis_2{1, i})
-        % Check to see if the bin in hypothesis_2 is in hypothesis_1
-        is_in_hypothesis_1 = false;
-        for k = 1:length(hypothesis_1{1, i})
-            if isequal(hypothesis_1{1, i}{1, k}, hypothesis_2{1,i}{1,j})
-                is_in_hypothesis_1 = true;
-                break
-            end
-        end
-        
-        if ~is_in_hypothesis_1
-            is_general = false;
-            return;
-        end
+    % For the bin in hypothesis_2
+    % Check to see if the bin in hypothesis_2 is in hypothesis_1
+    if isequal(hypothesis_1{1, i}{1,1}, -1)
+        continue;
+    elseif isequal(hypothesis_1{1, i}{1,1}, hypothesis_2{1,i}{1,1})
+        continue;
     end
+    is_general = false;
+    return;
 end
 
 is_general = true;
